d1cd9e4ae5f57c7fb29da1b723f3ea83929d488a
Mohamed Boudjenane <mohaboudjenane@gmail.com>
2021-03-01 16:38:58 +0100

    added CD to deploy to Heroku

--- mlproject/distance.py
+++ mlproject/distance.py
@@ -2,7 +2,7 @@ from math import radians, cos, sin, asin, sqrt
 
 def haversine(lon1, lat1, lon2, lat2):
     """
-    Calculate the great circle distance between two points
+    Calculate  the great circle distance between two points
     on the earth (specified in decimal degrees)
     """
     # convert decimal degrees to radians
